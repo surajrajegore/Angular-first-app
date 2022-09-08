f0f92c38c4eb2c4ad79c14abad45bd86aaa12e79
surajrajegore <rajegoresuraj2880@gmail.com>
2022-09-08 09:31:33 +0530

    Added:code of one way data binding using interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = "Hello World";
+  
+  ngOnInit():void{
+    this.title = "Hello from BridgeLabz.";
+  }
 }
