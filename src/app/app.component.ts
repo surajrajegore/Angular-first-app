feb85025d35f723c61f7430bc12f709d0aea40cd
surajrajegore <rajegoresuraj2880@gmail.com>
2022-09-08 11:42:50 +0530

    Added:code of validate username using event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,24 @@ export class AppComponent {
   imgAlt = "this is the bridgelabz logo"
   imgTitle = "bridgelabz"
 
-  userName:String = "";
+  userName:string = "";
+  userNameError = "";
 
   url = "https://bridgelabz.com/"
   ngOnInit():void{
     this.title = "Hello from BridgeLabz.";
   }
   onClick($event:any){
-    console.log("image is clicked",$event)
-    window.open(this.url,"_blank")
+    console.log("image is clicked",$event);
+    window.open(this.url,"_blank");
+  }
+  onInput($event:any,){
+    console.log("Change Event",$event.data);
+    const userNameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
+    if(userNameRegex.test(this.userName)){
+      this.userNameError="";
+      return;
+    }
+    this.userNameError="Invalid username  "
   }
 }
